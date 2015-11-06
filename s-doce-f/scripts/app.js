05b8ed20476afba2659117506b81be52e3fea3ef
Marcelo Fernandez Guerrero <mfernandez@adsib.gob.bo>
2015-11-06 11:17:29 -0400

    xx 6 11 2015 hasta plantillas

--- s-doce-f/scripts/app.js
+++ s-doce-f/scripts/app.js
@@ -1,101 +1,178 @@
 
-var app = angular.module('appMaestras', ['ngRoute', 'ngResource', 'hc.marked'])
-.config(['$routeProvider', function($routeProvider)
-    {
-        $routeProvider.when('/maestras', {
-            templateUrl: "templates/maestras-lista.html",
-            controller: "inicioCtrl"
-        });
-        $routeProvider.when('/maestras/nueva', {
-            templateUrl: "forms/maestrasForm.html",
-//                    templateUrl: "templates/maestras-nueva.html",
-            controller: "nuevoCtrl"
-        });
-        $routeProvider.when('/maestras/editar/:id', {
-            templateUrl: "templates/maestras-editar.html",
-            controller: "editCtrl"
-        });
-        $routeProvider.when('/maestras/x', {
-            templateUrl: "templates/pruebas.html",
-            controller: "pruebaCtrl"
-        });
-        $routeProvider.otherwise({
-            redirectTo: '/maestras'
-        });
-    }])
-.config(['markedProvider', function(markedProvider)
-    {
-        markedProvider.setOptions({gfm: true});
-    }])
+    var app = angular.module('appMaestras', ['ngRoute', 'ngResource', 'hc.marked'])
+        .config(['markedProvider', function(markedProvider)
+            {
+                markedProvider.setOptions({gfm: true});
+            }])
+        .factory('appFactory', function($resource, $location)
+        {
+            var comun = {};
+            comun.restMaestras = $resource("../s-doce-b/public/index.php/maestras/:id", {id: "@_id"}, {
+                update: {method: "PUT", params: {id: "@id"}} 
+            });
 
-.factory('maestrasFactory', function($resource, $location)
-{
-    var comun = {};
-    comun.resource = $resource("../s-doce-b/public/index.php/maestras/:id", {id: "@_id"}, {
-        update: {method: "PUT", params: {id: "@id"}}
-    });
-    comun.irInicio = function() {
-        $location.url("/maestras");
-    };
-    return comun;
-})
+            comun.restPlantillas = $resource("../s-doce-b/public/index.php/plantillas/:id", {id: "@_id"}, {
+                update: {method: "PUT", params: {id: "@id"}}
+            });
 
-.run(function($rootScope) {
-    $rootScope.markupSettings = settingsMarkdown;
+//            comun.irInicio = function() {
+//                $location.url("/maestras");
+//            };
+            return comun;
+        })
 
-})
+        .run(function($rootScope) {
+            $rootScope.markupSettings = settingsMarkdown;
+
+        })
+        .directive("markitup", function() {
+            return {
+                restrict: "A",
+                scope: {
+                    settings: "=markitup"
+                },
+                link: function(scope, element, attrs) {
+                    angular.element(element).markItUp(scope.settings);
+                }
+            };
+        });
+
+    /*
+     * Rutas  y Controladores de MAESTRAS ===================================================================
+     * 
+     */
+
+    app.config(['$routeProvider', function($routeProvider)
+        {
+            $routeProvider.when('/maestras', {
+                templateUrl: "templates/maestras-lista.html",
+                controller: "inicioCtrl"
+            });
+            $routeProvider.when('/maestras/nueva', {
+                templateUrl: "templates/maestras-nueva.html",
+                controller: "nuevoCtrl"
+            });
+            $routeProvider.when('/maestras/editar/:id', {
+                templateUrl: "templates/maestras-editar.html",
+                controller: "editCtrl"
+            });
+            $routeProvider.when('/x', {
+                templateUrl: "templates/pruebas.html",
+                controller: "pruebaCtrl"
+            });
 
 
-.controller('inicioCtrl', function($scope, maestrasFactory)
-{
-    maestrasFactory.resource.get(function(respuesta) {
-        $scope.lista = respuesta.plantillas_maestras;
-    });
-})
-.controller('nuevoCtrl', function($scope, maestrasFactory)
-{
+            $routeProvider.otherwise({
+                redirectTo: '/maestras'
+            });
+        }])
+        .controller('inicioCtrl', function($scope, appFactory)
+        {
+            appFactory.restMaestras.get(function(respuesta) {
+                $scope.lista = respuesta.plantillas_maestras;
+            });
+        })
+        .controller('nuevoCtrl', function($scope, appFactory)
+        {
 //    $scope.markupSettings = settingsMarkdown;
-    $scope.maestra = {};
-    $scope.guardar = function() {
-        maestrasFactory.resource.save($scope.maestra).$promise.then(function(respuesta)
-        {
-            if (respuesta.mensaje)
-            {
+            $scope.maestra = {};
+            $scope.guardar = function() {
+                appFactory.restMaestras.save($scope.maestra).$promise.then(function(respuesta)
+                {
+                    if (respuesta.mensaje)
+                    {
 
-            }
-        });
-    };
-})
-.controller('editCtrl', function($scope, maestrasFactory, $routeParams)
-{
-    var id = $routeParams.id;
-    maestrasFactory.resource.get({id: id}, function(data) {
-        $scope.maestra = data.plantilla_maestra;
-    });
-    $scope.maestra = {};
-    $scope.guardar = function()
-    {
-        maestrasFactory.resource.update({id: $scope.maestra._id}, $scope.maestra)
-        .$promise.then(function(respuesta)
+                    }
+                });
+            };
+        })
+        .controller('editCtrl', function($scope, appFactory, $routeParams)
         {
-            if (respuesta.mensaje)
+            var id = $routeParams.id;
+            appFactory.restMaestras.get({id: id}, function(data) {
+                $scope.maestra = data.plantilla_maestra;
+            });
+            $scope.maestra = {};
+            $scope.guardar = function()
             {
+                appFactory.restMaestras.update({id: $scope.maestra._id}, $scope.maestra)
+                    .$promise.then(function(respuesta)
+                    {
+                        if (respuesta.mensaje)
+                        {
 
-            }
+                        }
+                    });
+            };
         });
-    };
-})
-.directive("markitup", function() {
-    return {
-        restrict: "A",
-        scope: {
-            settings: "=markitup"
-        },
-        link: function(scope, element, attrs) {
-            angular.element(element).markItUp(scope.settings);
-        }
-    };
-});
+
+    /*
+     * Rutas  y Controladores de PLANTILLAS ============================================================
+     * 
+     */
+
+    app.config(['$routeProvider', function($routeProvider)
+        {
+            $routeProvider.when('/plantillas', {
+                templateUrl: "templates/plantillas-lista.html",
+                controller: "plantillainicioCtrl"
+            });
+            $routeProvider.when('/plantillas/nueva', {
+                templateUrl: "templates/plantillas-nueva.html",
+                controller: "plantillanuevoCtrl"
+            });
+            $routeProvider.when('/plantillas/editar/:id', {
+                templateUrl: "templates/plantillas-editar.html",
+                controller: "plantillaeditCtrl"
+            });
+
+        }])
+
+        .controller('plantillainicioCtrl', function($scope, appFactory)
+        {
+            appFactory.restPlantillas.get(function(respuesta) {
+                $scope.lista = respuesta.plantillas;
+            });
+        })
+        .controller('plantillanuevoCtrl', function($scope, appFactory)
+        {
+            $scope.plantilla = {};
+            $scope.guardar = function() {
+                appFactory.restPlantillas.save($scope.plantilla).$promise.then(function(respuesta)
+                {
+                    if (respuesta.mensaje)
+                    {
+
+                    }
+                });
+            };
+        })
+        .controller('plantillaeditCtrl', function($scope, appFactory, $routeParams)
+        {
+            var id = $routeParams.id;
+            appFactory.restPlantillas.get({id: id}, function(data) {
+                $scope.plantilla = data.plantilla;
+            });
+            $scope.plantilla = {};
+
+            $scope.guardar = function()
+            {
+                appFactory.restPlantillas.update({id: $scope.plantilla._id}, $scope.plantilla)
+                    .$promise.then(function(respuesta)
+                    {
+                        if (respuesta.mensaje)
+                        {
+
+                        }
+                    });
+            };
+        });
+
+
+
+
+
+
 
 
 
